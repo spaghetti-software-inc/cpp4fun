dcc4f993707bd0dad6a570352461b5d3286523cd
n00b101 <vaziraamir@gmail.com>
2025-02-14 15:22:24 -0500

    test framework

--- ci_test_example.sh
+++ ci_test_example.sh
@@ -11,5 +11,7 @@ rm -rf build
 conan install . --output-folder=build --build=missing
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
-cmake --build .
-./compressor
+cmake --build . -j20
+./tests
+./cpp4fun
+
--- test/test_main.cpp
+++
@@ -1,7 +0,0 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
-TEST_CASE("Example test case", "[example]") {
-    REQUIRE(1 + 1 == 2);
-    REQUIRE(2 * 2 == 4);
-}
