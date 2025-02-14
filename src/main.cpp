f7a74df9d02bd8459cb2887ae2651bbfa7517fde
n00b101 <vaziraamir@gmail.com>
2025-02-14 15:27:16 -0500

    C++17

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,30 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-
+#include <iostream>
+#include <string>
+#include <vector>
 #include <zlib.h>
 
-int main(void) {
-    char buffer_in [256] = {"Conan is a MIT-licensed, Open Source package manager for C and C++ development, "
-                            "allowing development teams to easily and efficiently manage their packages and "
-                            "dependencies across platforms and build systems."};
-    char buffer_out [256] = {0};
+int main() {
+    std::string input = "Conan is a MIT-licensed, Open Source package manager for C and C++ development, "
+                        "allowing development teams to easily and efficiently manage their packages and "
+                        "dependencies across platforms and build systems.";
+    std::vector<char> output(256);
 
     z_stream defstream;
     defstream.zalloc = Z_NULL;
     defstream.zfree = Z_NULL;
     defstream.opaque = Z_NULL;
-    defstream.avail_in = (uInt) strlen(buffer_in);
-    defstream.next_in = (Bytef *) buffer_in;
-    defstream.avail_out = (uInt) sizeof(buffer_out);
-    defstream.next_out = (Bytef *) buffer_out;
+    defstream.avail_in = static_cast<uInt>(input.size());
+    defstream.next_in = reinterpret_cast<Bytef*>(input.data());
+    defstream.avail_out = static_cast<uInt>(output.size());
+    defstream.next_out = reinterpret_cast<Bytef*>(output.data());
 
     deflateInit(&defstream, Z_BEST_COMPRESSION);
     deflate(&defstream, Z_FINISH);
     deflateEnd(&defstream);
 
-    printf("Uncompressed size is: %lu\n", strlen(buffer_in));
-    printf("Compressed size is: %lu\n", defstream.total_out);
-
-    printf("ZLIB VERSION: %s\n", zlibVersion());
+    std::cout << "Uncompressed size is: " << input.size() << '\n';
+    std::cout << "Compressed size is: " << defstream.total_out << '\n';
+    std::cout << "ZLIB VERSION: " << zlibVersion() << '\n';
 
     return EXIT_SUCCESS;
 }
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,11 @@
 #include <gtest/gtest.h>
 
+#include <zlib.h>
+
+#include <array>
+#include <string>
+#include <iostream>
+
 // Demonstrate some basic assertions.
 TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
@@ -8,3 +14,28 @@ TEST(HelloTest, BasicAssertions) {
   EXPECT_EQ(7 * 6, 42);
 }
 
+TEST(ZLIB, BasicCompression) {
+  std::string input = "Conan is a MIT-licensed, Open Source package manager for C and C++ development, "
+                      "allowing development teams to easily and efficiently manage their packages and "
+                      "dependencies across platforms and build systems.";
+  std::array<char, 256> buffer_out = {0};
+
+  z_stream defstream;
+  defstream.zalloc = Z_NULL;
+  defstream.zfree = Z_NULL;
+  defstream.opaque = Z_NULL;
+  defstream.avail_in = static_cast<uInt>(input.size());
+  defstream.next_in = reinterpret_cast<Bytef*>(input.data());
+  defstream.avail_out = static_cast<uInt>(buffer_out.size());
+  defstream.next_out = reinterpret_cast<Bytef*>(buffer_out.data());
+
+  deflateInit(&defstream, Z_BEST_COMPRESSION);
+  deflate(&defstream, Z_FINISH);
+  deflateEnd(&defstream);
+
+  std::cout << "Uncompressed size is: " << input.size() << '\n';
+  std::cout << "Compressed size is: " << defstream.total_out << '\n';
+  std::cout << "ZLIB VERSION: " << zlibVersion() << '\n';
+
+  EXPECT_GT(input.size(), defstream.total_out);
+}
