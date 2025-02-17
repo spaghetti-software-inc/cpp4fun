f07d156d6c0028e9e0134e6d0c348025c44b4aef
n00b101 <vaziraamir@gmail.com>
2025-02-17 08:01:15 -0500

    ci

--- ci_test_example.sh
+++ ci_test_example.sh
@@ -6,8 +6,8 @@ set -x
 BASEDIR=$(dirname "$0")
 pushd "$BASEDIR"
 
-rm -rf build
-conan install . --output-folder=build --build=missing 
+#rm -rf build
+#conan install . --output-folder=build --build=missing 
 
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
